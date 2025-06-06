ae970dc3caf4a1c8cda41cf44a4779890cc992ac
Soubhik <soubhikpramanik10@gmail.com>
2025-06-06 12:20:07 +0530

    text

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Hello world!");
+
+
+const data = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, ad quisquam excepturi sint perspiciatis voluptate velit maxime consequuntur sapiente molestias iusto inventore dolore quaerat quod praesentium aspernatur ratione ab obcaecati?"
